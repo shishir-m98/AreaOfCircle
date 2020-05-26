7150e84cd262dd093d7a0c70766580e834a7565a
Shishir Mishra <62663732+shishir-m98@users.noreply.github.com>
2020-05-27 02:46:00 +0530

    Update Circle.py

--- Circle.py
+++ Circle.py
@@ -1,3 +1,20 @@
+TASK 1:
+  
 r = float(input("Input the radius of circle: "))
 area = (22/7) * r ** 2
 print("The area of the crircle with radius", r, "is", area)
+
+OUTPUT
+The area of the circle with radius 1.1 is: 3.801327110843
+  
+........................................................................................................................................
+ TASK 2
+  
+file_name = input("Enter a file name: ")
+file_extension = file_name.split(".")
+print("The extension of the file is: ", repr(file_name[-2:]))
+
+OUTPUT
+The extension of the file is:  'py'
+ 
+  
